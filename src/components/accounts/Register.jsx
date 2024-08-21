bed47454e09f63f08bf6234d5ec07c28280f8c6d
tomason182 <elotro182@hotmail.com>
2024-08-21 20:08:10 -0300

    add: CSS normalization and variables

---
+++ src/components/accounts/Register.jsx
@@ -0,0 +1,12 @@
+function Register() {
+  return (
+    <div className="register-container">
+      <h2>Sign Up</h2>
+      <form action="" className="register-form">
+        <div className="form-container"></div>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
