0762502243aa7b4611198fc044576c606efbf5b9
tomason182 <elotro182@hotmail.com>
2024-08-31 17:26:59 -0300

    remove: Flex from defualt form

--- src/components/main_app/property/PropertyDetails.jsx
+++ src/components/main_app/property/PropertyDetails.jsx
@@ -29,82 +29,80 @@ function PropertyDetails() {
   };
 
   return (
-    <div id="mainContent">
-      <form className={styles.mainForm}>
-        <fieldset>
-          <legend>Property name</legend>
-          <label htmlFor="property_name" className={styles.hidden}>
-            Property name:
-          </label>
-          <input
-            type="text"
-            id="property_name"
-            aria-required
-            minLength={2}
-            maxLength={50}
-            defaultValue={propertyDetails.property_name}
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <legend>Contact info</legend>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            defaultValue={propertyDetails.contact_info.email}
-          />
-          <label htmlFor="phone">Phone number</label>
-          <input
-            type="text"
-            name="phoneNumber"
-            id="phone"
-            defaultValue={propertyDetails.contact_info.phone_number}
-          />
-        </fieldset>
-        <fieldset>
-          <legend>Address</legend>
-          <label htmlFor="street">Street</label>
-          <input
-            type="text"
-            name="street"
-            id="street"
-            maxLength={50}
-            defaultValue={propertyDetails.address.street}
-          />
-          <label htmlFor="city">City</label>
-          <input
-            type="text"
-            name="city"
-            id="city"
-            defaultValue={propertyDetails.address.city}
-          />
-          <label htmlFor="postal_code">Postal Code</label>
-          <input
-            type="text"
-            name="postalCode"
-            id="postal_code"
-            defaultValue={propertyDetails.address.postal_code}
-          />
-          <label htmlFor="country_code">Country code</label>
-          <input
-            type="text"
-            name="countryCode"
-            id="country_code"
-            defaultValue={propertyDetails.address.country_code}
-          />
-        </fieldset>
-        <menu className={styles.buttonContainer}>
-          <button type="reset" className={styles.resetBtn}>
-            Reset
-          </button>
-          <button type="submit" className={styles.submitBtn}>
-            Save changes
-          </button>
-        </menu>
-      </form>
-    </div>
+    <form className={styles.mainForm}>
+      <fieldset>
+        <legend>Property name</legend>
+        <label htmlFor="property_name" className={styles.hidden}>
+          Property name:
+        </label>
+        <input
+          type="text"
+          id="property_name"
+          aria-required
+          minLength={2}
+          maxLength={50}
+          defaultValue={propertyDetails.property_name}
+          required
+        />
+      </fieldset>
+      <fieldset>
+        <legend>Contact info</legend>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          defaultValue={propertyDetails.contact_info.email}
+        />
+        <label htmlFor="phone">Phone number</label>
+        <input
+          type="text"
+          name="phoneNumber"
+          id="phone"
+          defaultValue={propertyDetails.contact_info.phone_number}
+        />
+      </fieldset>
+      <fieldset>
+        <legend>Address</legend>
+        <label htmlFor="street">Street</label>
+        <input
+          type="text"
+          name="street"
+          id="street"
+          maxLength={50}
+          defaultValue={propertyDetails.address.street}
+        />
+        <label htmlFor="city">City</label>
+        <input
+          type="text"
+          name="city"
+          id="city"
+          defaultValue={propertyDetails.address.city}
+        />
+        <label htmlFor="postal_code">Postal Code</label>
+        <input
+          type="text"
+          name="postalCode"
+          id="postal_code"
+          defaultValue={propertyDetails.address.postal_code}
+        />
+        <label htmlFor="country_code">Country code</label>
+        <input
+          type="text"
+          name="countryCode"
+          id="country_code"
+          defaultValue={propertyDetails.address.country_code}
+        />
+      </fieldset>
+      <menu className={styles.buttonContainer}>
+        <button type="reset" className={styles.resetBtn}>
+          Reset
+        </button>
+        <button type="submit" className={styles.submitBtn}>
+          Save changes
+        </button>
+      </menu>
+    </form>
   );
 }
 
