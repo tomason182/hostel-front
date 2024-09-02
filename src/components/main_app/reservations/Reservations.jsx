0f1986da2c35b9f795333731167b73a38194979a
tomason182 <elotro182@hotmail.com>
2024-09-02 19:16:17 -0300

    add: Reservations Page

--- src/components/main_app/reservations/Reservations.jsx
+++ src/components/main_app/reservations/Reservations.jsx
@@ -1,5 +1,33 @@
+import styles from "../../../styles/Reservations.module.css";
+import { reservations } from "../../../data_mocked";
 function Reservations() {
-  return <h1>Reservations Related Content</h1>;
+  const listItems = reservations.map(reservation => (
+    <li key={reservation._id}>
+      <p>{reservation._id}</p>
+      <p>{reservation.guest_id}</p>
+      <p className={styles.dates}>{reservation.check_in_date}</p>
+      <p className={styles.dates}>{reservation.check_out_date}</p>
+      <p className={styles.prices}>$&nbsp;{reservation.total_price}</p>
+    </li>
+  ));
+
+  return (
+    <>
+      <h3 className={styles.title}>Reservations</h3>
+      <hr className={styles.solid} />
+      <form className={styles.form}>
+        <label htmlFor="from">From</label>
+        <input type="date" id="from" name="from_date" />
+        <label htmlFor="until">Until</label>
+        <input type="date" id="until" name="until_date" />
+        <label htmlFor="search">Search</label>
+        <input type="text" id="search" name="search" />
+        <button type="submit">Search</button>
+      </form>
+
+      <ul className={styles.reservationsList}>{listItems}</ul>
+    </>
+  );
 }
 
 export default Reservations;
