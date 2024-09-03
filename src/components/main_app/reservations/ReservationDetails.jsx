d482235941c140006f6a1ab257d066ce4c95daa7
tomason182 <elotro182@hotmail.com>
2024-09-03 12:15:38 -0300

    add: Style Reservation details page

--- src/components/main_app/reservations/ReservationDetails.jsx
+++ src/components/main_app/reservations/ReservationDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-
+import styles from "../../../styles/ReservationDetails.module.css";
 import { reservations } from "../../../data_mocked";
 
 export default function ReservationDetails() {
@@ -10,31 +10,68 @@ export default function ReservationDetails() {
   );
 
   return (
-    <>
-      <div className="reservationDetails">
-        <h3>{reservationData[0].guest_id}</h3>
-        <span>Arrival:</span>
-        <p>{reservationData[0].check_in_date}</p>
-        <span>Departure:</span>
-        <p>{reservationData[0].check_out_date}</p>
-        <p>Reservation Details</p>
-        <dl>
-          <dt>Room Type</dt>
-          <dd>{reservationData[0].room_type_id}</dd>
-          <dt>Booking Source</dt>
-          <dd>{reservationData[0].booking_source}</dd>
-          <dt>Number of guests</dt>
-          <dd>{reservationData[0].number_of_guest}</dd>
-          <dt>Total price</dt>
-          <dd>$&nbsp;{reservationData[0].total_price}</dd>
-          <dt>Reservation Status</dt>
-          <dd>{reservationData[0].reservation_status}</dd>
-          <dt>Payment Status</dt>
-          <dd>{reservationData[0].payment_status}</dd>
-          <dt>Special Request</dt>
-          <dd>{reservationData[0].special_request}</dd>
-        </dl>
+    <div className={styles.mainContainer}>
+      <div className={styles.reservationDetails}>
+        <div className={styles.one}>
+          <h3>{reservationData[0].guest_id}</h3>
+          <span>Arrival:</span>
+          <p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#000000"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4M10 17l5-5-5-5M13.8 12H3" />
+            </svg>
+            {reservationData[0].check_in_date}
+          </p>
+          <span>Departure:</span>
+          <p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#000000"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M10 3H6a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h4M16 17l5-5-5-5M19.8 12H9" />
+            </svg>
+            {reservationData[0].check_out_date}
+          </p>
+        </div>
+        <div className={styles.two}>
+          <p>Reservation Details</p>
+          <dl>
+            <dt>Room Type</dt>
+            <dd>{reservationData[0].room_type_id}</dd>
+            <dt>Booking Source</dt>
+            <dd>{reservationData[0].booking_source}</dd>
+            <dt>Number of guests</dt>
+            <dd>{reservationData[0].number_of_guest}</dd>
+            <dt>Total price</dt>
+            <dd>$&nbsp;{reservationData[0].total_price}</dd>
+            <dt>Reservation Status</dt>
+            <dd>{reservationData[0].reservation_status}</dd>
+            <dt>Payment Status</dt>
+            <dd>{reservationData[0].payment_status}</dd>
+            <dt>Special Request</dt>
+            <dd>{reservationData[0].special_request}</dd>
+          </dl>
+        </div>
       </div>
-    </>
+      <div className={styles.controlPanel}>
+        <h1>Control panel</h1>
+      </div>
+    </div>
   );
 }
