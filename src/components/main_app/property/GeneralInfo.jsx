9d4a06bf8127f56b51773ffb30e138f86676580b
tomason182 <elotro182@hotmail.com>
2024-09-05 10:38:04 -0300

    add: MainContent as grid

--- src/components/main_app/property/GeneralInfo.jsx
+++ src/components/main_app/property/GeneralInfo.jsx
@@ -1,5 +1,26 @@
+import styles from "../../../styles/GeneralInfo.module.css";
+
 function GeneralInfo() {
-  return <h1>Property general info content</h1>;
+  return (
+    <>
+      <h3 className="dashboardTitles">General information</h3>
+      <hr className="solidBreakLine" />
+      <div className={styles.mainContainer}>
+        <div className={styles.subContainer}>
+          <h4>Property info</h4>
+        </div>
+        <div className={styles.subContainer}>
+          <h4>Room types</h4>
+        </div>
+        <div className={styles.subContainer}>
+          <h4>Users</h4>
+        </div>
+        <div className={styles.subContainer}>
+          <h4>Active rate plans</h4>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default GeneralInfo;
