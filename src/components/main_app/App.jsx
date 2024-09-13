72670a06c6a5cd2f10b2a3e68389cde9cf20d775
tomason182 <elotro182@hotmail.com>
2024-09-13 20:04:58 -0300

    fix: Move state out of header

--- src/components/main_app/App.jsx
+++ src/components/main_app/App.jsx
@@ -2,11 +2,28 @@ import { Outlet } from "react-router-dom";
 import HeaderMain from "./HeaderMain";
 import NavigationMain from "./NavigationMain";
 import MainMenuResponsive from "../menus/MainMenuResponsive";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/users/profile", {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then(response => response.json())
+      .then(data => setUser(data.msg))
+      .catch(err => console.error("Error fetching users data: ", err));
+  }, []);
+
   return (
     <>
-      <HeaderMain />
+      <HeaderMain user={user} />
       <NavigationMain />
       <MainMenuResponsive />
       <Outlet />
--- src/components/main_app/HeaderMain.jsx
+++ src/components/main_app/HeaderMain.jsx
@@ -3,55 +3,17 @@ import ToggleThemeBtn from "../buttons/ToggleThemeBtn";
 import NotificationBtn from "../buttons/NotificationBtn";
 import MainSearch from "../forms/MainSearch";
 import styles from "../../styles/HeaderMain.module.css";
-import { useEffect, useState } from "react";
-import useFetch from "../../hooks/useFetch";
+import PropTypes from "prop-types";
 
-function HeaderMain() {
-  const [name, setName] = useState(null);
-  const [propertyName, setPropertyName] = useState(null);
-
-  const { data, error, loading } = useFetch(
-    name === null
-      ? {
-          url: "http://localhost:5000/api/v1/users/profile",
-          options: {
-            mode: "cors",
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            credentials: "include",
-          },
-        }
-      : {
-          url: null,
-          options: null,
-        }
-  );
-
-  useEffect(() => {
-    if (data) {
-      const obj = data.msg;
-      const userName = obj.user_info["first_name"];
-      const propertyName = obj.property_name;
-      setName(userName);
-      setPropertyName(propertyName);
-    }
-    if (error) {
-      console.error("Error: ", error);
-      setName("User's default name");
-      setPropertyName("Property name");
-    }
-  }, [data, error]);
-
+function HeaderMain({ user }) {
   return (
     <header className={styles.header}>
       <h1>Page Logo</h1>
       <MainSearch />
       <div className={styles.info}>
         <div className={styles.hostelInfo}>
-          <h3>{loading ? "Loading property name" : propertyName}</h3>
-          <p>{loading ? "Loading user name" : name}</p>
+          <h3>{user ? user.property_name : "Loading user name"}</h3>
+          <p>{user ? user.user_info.first_name : "Guest"}</p>
         </div>
         <NotificationBtn />
         <ToggleThemeBtn />
@@ -60,5 +22,9 @@ function HeaderMain() {
     </header>
   );
 }
+
+HeaderMain.propTypes = {
+  user: PropTypes.object,
+};
 
 export default HeaderMain;
