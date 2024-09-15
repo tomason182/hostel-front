da34add5d2745880760a491dcd5281398e91a0a6
tomason182 <elotro182@hotmail.com>
2024-09-15 16:34:29 -0300

    fix login form fetching data

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,64 +1,53 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import useFetch from "../../hooks/useFetch";
+import { useState } from "react";
 import styles from "../../styles/SignUpForm.module.css";
 export default function LoginForm() {
-  const [formBody, setFormBody] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [submit, setSubmit] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    const { username, password } = e.target;
+    setLoading(true);
+    if (error !== null) {
+      setError(null);
+    }
 
-    setFormBody({
-      username: username.value,
-      password: password.value,
-    });
+    const formBody = {
+      username: e.target.username.value,
+      password: e.target.password.value,
+    };
 
-    setSubmit(true);
-    setErrorMessage(null);
+    const url = "http://localhost:5000/api/v1/users/auth";
+    const options = {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(formBody),
+    };
+
+    fetch(url, options)
+      .then(async response => {
+        if (response.status >= 400) {
+          const errorObj = await response.json();
+          const errorMessage = errorObj.msg ? errorObj?.msg : "Server Error";
+          throw new Error(errorMessage);
+        }
+
+        return response.json();
+      })
+      .then(response => setData(response))
+      .catch(err => setError(err.message))
+      .finally(setLoading(false));
   }
 
-  const { data, error, loading } = useFetch(
-    submit === true && formBody !== null
-      ? {
-          url: "http://localhost:5000/api/v1/users/auth",
-          options: {
-            mode: "cors",
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            credentials: "include",
-            body: JSON.stringify(formBody),
-          },
-        }
-      : { url: null, options: null }
-  );
-
-  useEffect(() => {
-    if (data) {
-      navigate("/app");
-    }
-
-    if (error) {
-      let errorMsg = null;
-      try {
-        const errorObj = JSON.parse(error.message);
-        errorMsg = errorObj.msg;
-      } catch (err) {
-        console.error(err);
-        errorMsg = "An unexpected error ocurred";
-      }
-
-      setErrorMessage(errorMsg);
-    }
-    setSubmit(false);
-  }, [data, error]);
+  if (data) return navigate("/app");
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
@@ -82,9 +71,9 @@ export default function LoginForm() {
         aria-required
         aria-labelledby="invalid"
       />
-      {errorMessage && (
+      {error && (
         <span id="invalid" className={styles.error}>
-          {errorMessage}
+          {error}
         </span>
       )}
 
