3aed2f7121999c75b73de40e36164f73a86d270c
tomason182 <elotro182@hotmail.com>
2024-09-16 13:08:46 -0300

    add: FetchData helper function

---
+++ src/utils/fetchDataHelper.js
@@ -0,0 +1,22 @@
+export default async function fetchDataHelper(url, options) {
+  let errors = [];
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      Array.isArray(errorResponse)
+        ? (errors = errorResponse)
+        : errors.push(
+            errorResponse.message || "An error ocurred during request"
+          );
+      return { data: null, errors };
+    }
+
+    const data = await response.json();
+    return { data, errors: null };
+  } catch (err) {
+    errors.push(err.message);
+    return { data: null, errors };
+  }
+}
