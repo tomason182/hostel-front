db974622dc6e2d7aabb4078527301e2782390667
tomason182 <elotro182@hotmail.com>
2024-09-16 10:55:00 -0300

    add: Fetch property data
    Fetch property data and pass it as prop to componenets

--- src/components/main_app/property/GeneralInfo.jsx
+++ src/components/main_app/property/GeneralInfo.jsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
 import { useRef } from "react";
 import styles from "../../../styles/GeneralInfo.module.css";
-import PropertyInfoSub from "./PropertyInfoSub";
 import RoomTypesSub from "./RoomTypesSub";
 import UsersSub from "./UsersSub";
-import PropertyInfoForm from "../../forms/PropertyInfoForm";
+import PropertyInfoSub from "./PropertyInfoSub";
+import PropertyDetails from "../../forms/PropertyDetails";
 import DialogHeader from "../../dialogs/DialogHeader";
 import ContentTitle from "../../headers/ContentTitle";
 import UserForm from "../../forms/UserForm";
+import useFetch from "../../../hooks/useFetch";
 
 function GeneralInfo() {
   const propertyDialog = useRef(null);
   const userDialog = useRef(null);
+
+  const url = import.meta.env.VITE_URL_BASE + "properties";
+  const options = {
+    mode: "cors",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  };
+  const { data, error, loading } = useFetch({ url, options });
 
   return (
     <div className="main-content">
@@ -19,7 +31,7 @@ function GeneralInfo() {
       <div className={styles.mainContainer}>
         <dialog ref={propertyDialog} className="dialog">
           <DialogHeader title={"Property details"} refProps={propertyDialog} />
-          <PropertyInfoForm refProps={propertyDialog} />
+          <PropertyDetails refProps={propertyDialog} data={data} />
         </dialog>
         <dialog ref={userDialog} className="dialog">
           <DialogHeader title={"Add a User"} refProps={userDialog} />
@@ -27,7 +39,7 @@ function GeneralInfo() {
         </dialog>
         <div className={styles.subContainer}>
           <h4>Property info</h4>
-          <PropertyInfoSub />
+          <PropertyInfoSub data={data} error={error} loading={loading} />
           <button
             className={styles.editBtn}
             onClick={() => propertyDialog.current?.showModal()}
