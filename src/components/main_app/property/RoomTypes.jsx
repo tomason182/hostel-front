67c892e9363c66060e9d9295b64d8da7d61277b0
tomason182 <elotro182@hotmail.com>
2024-09-19 19:11:11 -0300

    add: Refactor room type display and fetch data
    Display the room types as grid instead of table and fetch data ffrom server

--- src/components/main_app/property/RoomTypes.jsx
+++ src/components/main_app/property/RoomTypes.jsx
@@ -1,55 +1,69 @@
-import { roomTypes } from "../../../data_mocked.js";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import RoomTypeForm from "../../forms/RoomTypeForm.jsx";
 import styles from "../../../styles/RoomTypes.module.css";
 import DialogHeader from "../../dialogs/DialogHeader.jsx";
+import fetchDataHelper from "../../../utils/fetchDataHelper.js";
+import ErrorComponent from "../../error_page/ErrorComponent.jsx";
 
 function RoomTypes() {
   const dialogRef = useRef(null);
   const editRef = useRef(null);
 
   const [roomTypeData, setRoomTypeData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function handleRoomTypeData() {
+      try {
+        const url = import.meta.env.VITE_URL_BASE + "room-types";
+        const options = {
+          mode: "cors",
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        };
+
+        const { data, errors } = await fetchDataHelper(url, options);
+
+        if (data) {
+          console.log(data);
+          setRoomTypeData(data);
+          return;
+        }
+
+        if (errors) {
+          setError(errors);
+        }
+      } catch (err) {
+        console.error(err);
+        setError([{ msg: err.message || "Unexpected error occurred" }]);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    handleRoomTypeData();
+  }, []);
 
   function handleRoomSelection(id) {
-    const data = roomTypes.find(room => room._id === id);
+    const data = roomTypeData.find(room => room._id === id);
     setRoomTypeData(data);
   }
 
-  const tableRows = roomTypes.map(roomType => (
-    <tr key={roomType._id}>
-      <th scope="row">{roomType.description}</th>
-      <td>{roomType.type}</td>
-      <td>{roomType.gender}</td>
-      <td>{roomType.bathroom}</td>
-      <td>{roomType.max_occupancy}</td>
-      <td>{roomType.inventory}</td>
-      <td>{`$ ${roomType.base_rate}`}</td>
-      <td>{roomType.currency}</td>
-      <td>
-        <a
-          role="button"
-          tabIndex={0}
-          aria-label="Edit room type"
-          onClick={() => {
-            handleRoomSelection(roomType._id);
-            editRef.current?.showModal();
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            role="img"
-            className={styles.editBtn}
-          >
-            <polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon>
-            <line x1="3" y1="22" x2="21" y2="22"></line>
-          </svg>
-        </a>
-      </td>
-    </tr>
-  ));
+  if (loading) return <div>Loading...</div>;
+
+  if (error)
+    return (
+      <div>
+        <ErrorComponent errors={error} />
+      </div>
+    );
+
   return (
-    <div className={`${styles.roomTypeContainer} main-content`}>
+    <div className={`${styles.mainContainer} main-content`}>
       <dialog ref={dialogRef} className="dialog">
         <DialogHeader title={"Create room type"} refProps={dialogRef} />
         <RoomTypeForm refProps={dialogRef} data={null} />
@@ -65,23 +79,46 @@ function RoomTypes() {
       >
         Create Room Type
       </button>
-      <table id={styles.table}>
-        <caption>Room types</caption>
-        <thead>
-          <tr>
-            <th scope="col">Description</th>
-            <th scope="col">Type</th>
-            <th scope="col">gender</th>
-            <th scope="col">bathroom</th>
-            <th scope="col">Max occupancy</th>
-            <th scope="col">Inventory</th>
-            <th scope="col">Base rate</th>
-            <th scope="col">Currency</th>
-            <th scope="col">Edit</th>
-          </tr>
-        </thead>
-        <tbody>{tableRows}</tbody>
-      </table>
+      <div className={styles.roomTypesContainer}>
+        {roomTypeData.map(roomType => (
+          <div key={roomType._id} className={styles.roomsContainer}>
+            <h3>{roomType.description}</h3>
+            <dl className={styles.dlList}>
+              <dt>type</dt>
+              <dd>{roomType.type}</dd>
+              <dt>gender</dt>
+              <dd>{roomType.gender}</dd>
+              <dt>Max occupancy</dt>
+              <dd>{roomType.max_occupancy}</dd>
+              <dt>Inventory</dt>
+              <dd>{roomType.inventory}</dd>
+              <dt>Base rate</dt>
+              <dd>{`$ ${roomType.base_rate}`}</dd>
+              <dt>Currency</dt>
+              <dd>{roomType.currency}</dd>
+            </dl>
+            <a
+              role="button"
+              tabIndex={0}
+              aria-label="Edit room type"
+              onClick={() => {
+                handleRoomSelection(roomType._id);
+                editRef.current?.showModal();
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                role="img"
+                className={styles.editBtn}
+              >
+                <polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon>
+                <line x1="3" y1="22" x2="21" y2="22"></line>
+              </svg>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
