b120e53916513bb4a6dca7eb40bc5029962382a8
tomason182 <elotro182@hotmail.com>
2024-09-23 17:28:50 -0300

    add: Dialog for new reservations

--- src/components/main_app/calendar/CalendarMainPage.jsx
+++ src/components/main_app/calendar/CalendarMainPage.jsx
@@ -1,11 +1,31 @@
 import Calendar from "./Calendar";
 import CreateBtn from "../../buttons/BtnCreate";
 import styles from "../../../styles/CalendarMainPage.module.css";
+import DialogHeader from "../../dialogs/DialogHeader";
+import { useRef, useState } from "react";
 
 function CalendarMainPage() {
+  const dialogRef = useRef(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   return (
     <div className={styles.mainContent}>
-      <CreateBtn title={"New reservation"} />
+      <dialog ref={dialogRef} className="dialog">
+        {isDialogOpen && (
+          <>
+            <DialogHeader
+              title={"Add new reservation"}
+              refProps={dialogRef}
+              setIsDialogOpen={setIsDialogOpen}
+            />
+          </>
+        )}
+      </dialog>
+      <CreateBtn
+        title={"New reservation"}
+        refProps={dialogRef}
+        setIsDialogOpen={setIsDialogOpen}
+      />
       <Calendar />
     </div>
   );
