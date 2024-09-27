fc38911cd2597f84bed8eede1247ff820f62e88d
tomason182 <elotro182@hotmail.com>
2024-09-27 12:56:56 -0300

    add: Handle reservations on calendar

--- src/components/main_app/calendar/Calendar.jsx
+++ src/components/main_app/calendar/Calendar.jsx
@@ -63,14 +63,67 @@ export default function Calendar() {
     </th>
   ));
 
-  const tableData = weeksArray.map(i => {
-    const currentDate = format(i, "yyyy-MM-dd");
+  function findReservation(day, bedId) {
+    const currentDate = format(day, "yyyy-MM-dd");
     const result = reservations.find(
-      r => format(r.check_in_date, "yyyy-MM-dd") === currentDate
+      r => r.check_in_date === currentDate && r.assignedBeds.includes(bedId)
     );
-    console.log(result);
-    return <td key={i}></td>;
-  });
+    if (!result) {
+      return null;
+    }
+
+    const daysDiff = Math.abs(
+      new Date(result.check_out_date) - new Date(result.check_in_date)
+    );
+    const nights = Math.ceil(daysDiff / (1000 * 60 * 60 * 24));
+    return { guestId: result.guest_id, nights };
+  }
+
+  function parseDate(dateString) {
+    const [year, month, day] = dateString.split("-").map(Number);
+    return new Date(year, month - 1, day);
+  }
+
+  function handleReservationOnStart(day, bedId) {
+    const currentDate = format(day, "yyyy-MM-dd");
+    const result = reservations.find(
+      r =>
+        r.check_in_date <= currentDate &&
+        r.check_out_date > currentDate &&
+        r.assignedBeds.includes(bedId)
+    );
+
+    if (!result) {
+      return null;
+    }
+
+    const daysDiff = Math.abs(
+      parseDate(result.check_out_date) - parseDate(currentDate)
+    );
+    const nights = daysDiff / (1000 * 60 * 60 * 24);
+    return { guestId: result.guest_id, nights };
+  }
+
+  function handleReservationOnEnd(day, bedId) {
+    const currentDate = format(day, "yyyy-MM-dd");
+    const result = reservations.find(
+      r =>
+        r.check_in_date <= currentDate &&
+        r.check_out_date > currentDate &&
+        r.assignedBeds.includes(bedId)
+    );
+
+    if (!result) {
+      return null;
+    }
+
+    const daysDiff = Math.abs(
+      parseDate(currentDate) - parseDate(result.check_in_date)
+    );
+    const nights = daysDiff / (1000 * 60 * 60 * 24);
+    return { guestId: result.guest_id, nights };
+  }
+
   const listOfRooms = roomTypes.map(room => (
     <Fragment key={room._id}>
       <tr className={styles.roomRow}>
@@ -83,12 +136,64 @@ export default function Calendar() {
               {product.room_name}
             </th>
           </tr>
-          {product.beds.map((bed, i) => (
-            <tr key={bed}>
-              <th className={styles.beds}>{i + 1}</th>
-              {tableData}
-            </tr>
-          ))}
+          {product.beds.map((bed, i) => {
+            let skipDays = 0;
+
+            return (
+              <tr key={bed}>
+                <th className={styles.beds}>{i + 1}</th>
+
+                {weeksArray.map((value, index) => {
+                  if (skipDays > 0) {
+                    skipDays -= 1;
+                    return null;
+                  }
+
+                  if (index === 0) {
+                    const hasReservation = handleReservationOnStart(value, bed);
+                    if (hasReservation) {
+                      skipDays = hasReservation.nights - 1;
+                    }
+                    return hasReservation ? (
+                      <td colSpan={hasReservation.nights}>
+                        {hasReservation.guestId}
+                      </td>
+                    ) : (
+                      <td></td>
+                    );
+                  } else if (index === weeksArray.length - 1) {
+                    const hasReservation = handleReservationOnEnd(value, bed);
+                    if (hasReservation) {
+                      skipDays = hasReservation.nights - 1;
+                    }
+                    return hasReservation ? (
+                      <td colSpan={hasReservation.nights}>
+                        {hasReservation.guestId}
+                      </td>
+                    ) : (
+                      <td></td>
+                    );
+                  } else {
+                    const hasReservation = findReservation(value, bed);
+                    let nights;
+                    if (hasReservation) {
+                      skipDays = hasReservation.nights - 1;
+
+                      index + hasReservation.nights > weeksArray.length
+                        ? (nights = weeksArray.length - index)
+                        : (nights = hasReservation.nights);
+                    }
+
+                    return hasReservation ? (
+                      <td colSpan={nights}>{hasReservation.guestId}</td>
+                    ) : (
+                      <td></td>
+                    );
+                  }
+                })}
+              </tr>
+            );
+          })}
         </Fragment>
       ))}
     </Fragment>
