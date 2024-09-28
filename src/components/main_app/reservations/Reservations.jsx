edbf3e048d8697b12ee506aea6bef12621a3af4f
tomason182 <elotro182@hotmail.com>
2024-09-28 17:06:25 -0300

    add: filter reservation by name

--- src/components/main_app/reservations/Reservations.jsx
+++ src/components/main_app/reservations/Reservations.jsx
@@ -1,27 +1,92 @@
 import styles from "../../../styles/Reservations.module.css";
 import { Link } from "react-router-dom";
 import ContentTitle from "../../headers/ContentTitle";
-import { reservations } from "../../../data_mocked";
+import { useState } from "react";
+import { format } from "date-fns";
 
 function Reservations() {
-  const listItems = reservations.map(reservation => (
-    <li key={reservation._id}>
-      <Link to={reservation._id}>{reservation._id}</Link>
-      <p>{reservation.guest_id}</p>
-      <p className={styles.dates}>{reservation.check_in_date}</p>
-      <p className={styles.dates}>{reservation.check_out_date}</p>
-      <p className={styles.prices}>$&nbsp;{reservation.total_price}</p>
-    </li>
-  ));
+  const [reservations, setReservations] = useState(null);
+
+  function findGuest(text, data) {
+    const names = text.toLowerCase().split(" ");
+    return data.filter(d => {
+      const fullName = d.guest_info.full_name;
+      const splitFullName = fullName.toLowerCase().split(" ");
+
+      return names.every(name => splitFullName.includes(name));
+    });
+  }
+
+  console.log(reservations);
+  async function fetchReservationData(e) {
+    e.preventDefault();
+
+    const fromDate = e.target.fromDate.value;
+    const toDate = e.target.untilDate.value;
+    const fullName = e.target.search.value;
+
+    console.log(fullName);
+
+    const formattedFormDate = format(fromDate, "yyyyMMdd");
+    const formattedToDate = format(toDate, "yyyyMMdd");
+
+    const url =
+      import.meta.env.VITE_URL_BASE +
+      "reservations/find/" +
+      formattedFormDate +
+      "-" +
+      formattedToDate;
+
+    const options = {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    };
+
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        if (fullName) {
+          const result = findGuest(fullName, data);
+          setReservations(result);
+        } else {
+          setReservations(data);
+        }
+      })
+      .catch(err =>
+        console.error("Error fetching reservations data", err.message)
+      );
+  }
+
+  const listItems =
+    reservations &&
+    reservations.map(reservation => (
+      <li key={reservation._id}>
+        <Link to={reservation._id}>
+          {reservation._id.substring(18).toUpperCase()}
+        </Link>
+        <p>{reservation.guest_info.full_name}</p>
+        <p className={styles.dates}>
+          {format(reservation.check_in, "yyyy-MM-dd")}
+        </p>
+        <p className={styles.dates}>
+          {format(reservation.check_out, "yyyy-MM-dd")}
+        </p>
+        <p className={styles.prices}>$&nbsp;{reservation.total_price}</p>
+      </li>
+    ));
 
   return (
     <div className="main-content">
       <ContentTitle title={"Reservations"} />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={fetchReservationData}>
         <label htmlFor="from">From</label>
-        <input type="date" id="from" name="from_date" />
+        <input type="date" id="from" name="fromDate" />
         <label htmlFor="until">Until</label>
-        <input type="date" id="until" name="until_date" />
+        <input type="date" id="until" name="untilDate" />
         <label htmlFor="search">Search</label>
         <input type="text" id="search" name="search" />
         <button type="submit">Search</button>
