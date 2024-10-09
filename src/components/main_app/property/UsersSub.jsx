eb75551633b1353c44d2b0e1db82666201008ae7
tomason182 <elotro182@hotmail.com>
2024-10-09 20:27:48 -0300

    add delete btn on user sub

--- src/components/main_app/property/UsersSub.jsx
+++ src/components/main_app/property/UsersSub.jsx
@@ -25,7 +25,7 @@ export default function UsersSub({
         <div className={styles.info}>
           <span>{user.role}</span>
         </div>
-        <div>
+        <div className={styles.btnMenu}>
           <button
             onClick={() => {
               setUserValues({
@@ -38,7 +38,36 @@ export default function UsersSub({
               refProps.current?.showModal();
             }}
           >
-            Edit
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon>
+              <line x1="3" y1="22" x2="21" y2="22"></line>
+            </svg>
+          </button>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <polyline points="3 6 5 6 21 6"></polyline>
+              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+              <line x1="10" y1="11" x2="10" y2="17"></line>
+              <line x1="14" y1="11" x2="14" y2="17"></line>
+            </svg>
           </button>
         </div>
       </li>
