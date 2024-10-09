5dcd952156dc00926b04e9a6a07a07d2c1df24a8
tomason182 <elotro182@hotmail.com>
2024-10-09 10:31:58 -0300

    add: style to guest control panel

--- src/components/main_app/guest/GuestControlPanel.jsx
+++ src/components/main_app/guest/GuestControlPanel.jsx
@@ -1,5 +1,6 @@
 import styles from "../../../styles/ReservationControlPanel.module.css";
 import GuestFormUpdate from "../../forms/GuestFormUpdate";
+import { Link } from "react-router-dom";
 import { useRef, useState } from "react";
 import PropTypes from "prop-types";
 
@@ -18,6 +19,7 @@ export default function GuestControlPanel({ guestData, setToggleDisplay }) {
         )}
       </dialog>
       <div className={styles.controlPanel}>
+        <p>Update this guest</p>
         <button
           className={styles.btnLarge}
           onClick={() => {
@@ -27,9 +29,29 @@ export default function GuestControlPanel({ guestData, setToggleDisplay }) {
         >
           Update Guest Information
         </button>
-        <button className={styles.btnLarge} onClick={() => setToggleDisplay(1)}>
-          Go back to reservations
-        </button>
+        <button className={styles.btnLarge}>Delete Guest</button>
+        <Link
+          className={styles.goBackLink}
+          to="#"
+          role="button"
+          onClick={() => setToggleDisplay(1)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#000000"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <polygon points="19 20 9 12 19 4 19 20"></polygon>
+            <line x1="5" y1="19" x2="5" y2="5"></line>
+          </svg>
+          Back
+        </Link>
       </div>
     </>
   );
