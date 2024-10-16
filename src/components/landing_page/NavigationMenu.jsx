cca822c9376a4ea599816d28b5a422d26a1df4d2
tomason182 <elotro182@hotmail.com>
2024-10-16 19:03:58 -0300

    add: check for auth on landing page

--- src/components/landing_page/NavigationMenu.jsx
+++ src/components/landing_page/NavigationMenu.jsx
@@ -1,7 +1,20 @@
 import styles from "../../styles/HeaderLanding.module.css";
+import isAuthenticated from "../../hooks/isAuthenticated";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function NavigationMenu() {
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    async function checkForAuth() {
+      const response = await isAuthenticated();
+      setIsAuth(response);
+    }
+
+    checkForAuth();
+  }, []);
+
   return (
     <nav className={styles.nav} role="navigation">
       <Link to="/">Home</Link>
@@ -9,10 +22,18 @@ function NavigationMenu() {
       <Link to="#">Pricing</Link>
       <Link to="#">Contact</Link>
       <Link to="#">About us</Link>
-      <Link to="/accounts/login"> Sign in</Link>
-      <Link to="/accounts/signup" className={styles.btnPrimary}>
-        Create Account
-      </Link>
+      {isAuth ? (
+        <Link to="/app" className={styles.btnPrimary}>
+          Go to app
+        </Link>
+      ) : (
+        <>
+          <Link to="/accounts/login"> Sign in</Link>
+          <Link to="/accounts/signup" className={styles.btnPrimary}>
+            Create Account
+          </Link>
+        </>
+      )}
     </nav>
   );
 }
