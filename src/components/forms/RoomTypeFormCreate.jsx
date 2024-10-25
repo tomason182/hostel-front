f63eae860f436d9b6e9e9a614f630854221aeb05
tomason182 <elotro182@hotmail.com>
2024-10-25 10:58:27 -0300

    add: style create roomType form

--- src/components/forms/RoomTypeFormCreate.jsx
+++ src/components/forms/RoomTypeFormCreate.jsx
@@ -1,5 +1,5 @@
-import stylesForm from "../../styles/formDefaultStyle.module.css";
-import styles from "../..//styles/RoomTypeForm.module.css";
+import styles from "../../styles/formDefaultStyle.module.css";
+/* import styles from "../..//styles/RoomTypeForm.module.css"; */
 import PropTypes from "prop-types";
 import { useState } from "react";
 import fetchDataHelper from "../../utils/fetchDataHelper";
@@ -16,9 +16,6 @@ export default function RoomTypesFormCreate({
   async function handleSubmit(e) {
     e.preventDefault();
     setLoading(true);
-
-    /*    const formBody = e.target;
-    const formData = new FormData(formBody); */
 
     const {
       description,
@@ -74,175 +71,184 @@ export default function RoomTypesFormCreate({
   }
 
   return (
-    <form
-      id={styles.form}
-      className={stylesForm.mainForm}
-      method="dialog"
-      onSubmit={handleSubmit}
-    >
-      <section>
-        <label>
-          Room type name
+    <form className={styles.mainForm} method="dialog" onSubmit={handleSubmit}>
+      <label>
+        Room type name
+        <input
+          type="text"
+          name="description"
+          minLength={1}
+          maxLength={100}
+          placeholder="e.g. Bed in 6max mixed dormitory"
+          required
+          aria-required
+        />
+      </label>
+      <fieldset>
+        <legend>Room type selection: Dormitory or Private</legend>
+        <div className={styles.radioContainer}>
+          <label>
+            Private
+            <input
+              type="radio"
+              name="type"
+              defaultValue="private"
+              defaultChecked
+            />
+          </label>
+        </div>
+        <div className={styles.radioContainer}>
+          <label>
+            Dormitory
+            <input type="radio" name="type" defaultValue="dorm" />
+          </label>
+        </div>
+      </fieldset>
+      <fieldset>
+        <legend>Gender</legend>
+        <div className={styles.radioContainer}>
+          <label>
+            Mixed
+            <input
+              type="radio"
+              name="gender"
+              defaultValue="mixed"
+              defaultChecked
+            />
+          </label>
+        </div>
+        <div className={styles.radioContainer}>
+          <label>
+            Female
+            <input type="radio" name="gender" defaultValue="female" />
+          </label>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend>Room Capacity & Pricing Details</legend>
+        <label className={styles.infoLabel}>
+          MaxOccupancy
+          <span className={styles.tooltipContainer}>
+            <svg
+              className={styles.infoIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              aria-describedby="maxOccupancyInfo"
+              role="img"
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <line x1="12" y1="16" x2="12" y2="12"></line>
+              <line x1="12" y1="8" x2="12.01" y2="8"></line>
+            </svg>
+            <span
+              id="maxOccupancyInfo"
+              role="tooltip"
+              className={styles.tooltip}
+            >
+              Maximum number of guests allowed in the room
+            </span>
+          </span>
           <input
-            type="text"
-            name="description"
-            minLength={1}
-            maxLength={100}
+            type="number"
+            name="max_occupancy"
             required
             aria-required
+            min={1}
+            max={20}
           />
         </label>
-        <div className={styles.flexContainer}>
-          <fieldset>
-            <legend>Room type selection: Dormitory or Private</legend>
-            <div className={styles.radioInputContainer}>
-              <label>
-                Private
-                <input
-                  type="radio"
-                  name="type"
-                  defaultValue="private"
-                  defaultChecked
-                />
-              </label>
-            </div>
-            <div className={styles.radioInputContainer}>
-              <label>
-                Dormitory
-                <input type="radio" name="type" defaultValue="dorm" />
-              </label>
-            </div>
-          </fieldset>
-          <fieldset>
-            <legend>Gender</legend>
-            <div className={styles.radioInputContainer}>
-              <label>
-                Mixed
-                <input
-                  type="radio"
-                  name="gender"
-                  defaultValue="mixed"
-                  defaultChecked
-                />
-              </label>
-            </div>
-            <div className={styles.radioInputContainer}>
-              <label>
-                Female
-                <input type="radio" name="gender" defaultValue="female" />
-              </label>
-            </div>
-          </fieldset>
-        </div>
-        <fieldset>
-          <legend>Room Capacity & Pricing Details</legend>
-          <div className={styles.gridContainer}>
-            <label className={styles.labelContainer}>
-              MaxOccupancy
-              <span className={styles.tooltipContainer}>
-                <svg
-                  className={styles.infoIcon}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  aria-describedby="maxOccupancyInfo"
-                  role="img"
-                >
-                  <circle cx="12" cy="12" r="10"></circle>
-                  <line x1="12" y1="16" x2="12" y2="12"></line>
-                  <line x1="12" y1="8" x2="12.01" y2="8"></line>
-                </svg>
-                <span
-                  id="maxOccupancyInfo"
-                  role="tooltip"
-                  className={styles.tooltip}
-                >
-                  Maximum number of guests allowed in the room
-                </span>
-              </span>
-              <input
-                type="number"
-                name="max_occupancy"
-                required
-                aria-required
-                min={1}
-                max={20}
-              />
-            </label>
-            <label className={styles.labelContainer}>
-              Inventory
-              <span className={styles.tooltipContainer}>
-                <svg
-                  className={styles.infoIcon}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  aria-describedby="inventory"
-                  role="img"
-                >
-                  <circle cx="12" cy="12" r="10"></circle>
-                  <line x1="12" y1="16" x2="12" y2="12"></line>
-                  <line x1="12" y1="8" x2="12.01" y2="8"></line>
-                </svg>
-                <span id="inventory" role="tooltip" className={styles.tooltip}>
-                  Number of room of the same type
-                </span>
-              </span>
-              <input
-                type="number"
-                name="inventory"
-                required
-                aria-required
-                min={1}
-                max={50}
-              />
-            </label>
-            <label className={styles.labelContainer}>
-              Base rate
-              <span className={styles.tooltipContainer}>
-                <svg
-                  className={styles.infoIcon}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  aria-labelledby="base_rate"
-                  role="img"
-                >
-                  <circle cx="12" cy="12" r="10"></circle>
-                  <line x1="12" y1="16" x2="12" y2="12"></line>
-                  <line x1="12" y1="8" x2="12.01" y2="8"></line>
-                </svg>
-                <span id="base_rate" role="tooltip" className={styles.tooltip}>
-                  Base rate per night and per guest
-                </span>
-              </span>
-              <input type="number" name="base_rate" required min={1} />
-            </label>
-            <label className={styles.labelContainer}>
-              Currency
-              <input type="text" name="currency" required min={1} />
-            </label>
-          </div>
-        </fieldset>
-      </section>
-      <menu className={stylesForm.buttonContainer}>
+        <label className={styles.infoLabel}>
+          Inventory
+          <span className={styles.tooltipContainer}>
+            <svg
+              className={styles.infoIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              aria-describedby="inventory"
+              role="img"
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <line x1="12" y1="16" x2="12" y2="12"></line>
+              <line x1="12" y1="8" x2="12.01" y2="8"></line>
+            </svg>
+            <span id="inventory" role="tooltip" className={styles.tooltip}>
+              Number of room of the same type
+            </span>
+          </span>
+          <input
+            type="number"
+            name="inventory"
+            required
+            aria-required
+            min={1}
+            max={50}
+          />
+        </label>
+        <label className={styles.infoLabel}>
+          Base rate
+          <span className={styles.tooltipContainer}>
+            <svg
+              className={styles.infoIcon}
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              aria-labelledby="base_rate"
+              role="img"
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <line x1="12" y1="16" x2="12" y2="12"></line>
+              <line x1="12" y1="8" x2="12.01" y2="8"></line>
+            </svg>
+            <span id="base_rate" role="tooltip" className={styles.tooltip}>
+              Base rate per night and per guest
+            </span>
+          </span>
+          <input type="number" name="base_rate" required min={1} />
+        </label>
+        <label className={styles.infoLabel}>
+          Currency
+          <svg
+            className={styles.infoIcon}
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            aria-labelledby="base_rate"
+            role="img"
+          >
+            <circle cx="12" cy="12" r="10"></circle>
+            <line x1="12" y1="16" x2="12" y2="12"></line>
+            <line x1="12" y1="8" x2="12.01" y2="8"></line>
+          </svg>
+          <input type="text" name="currency" required min={1} />
+        </label>
+      </fieldset>
+
+      <menu className={styles.buttonContainer}>
         <button
-          className={stylesForm.resetBtn}
+          className={styles.resetBtn}
           type="reset"
           disabled={loading}
           onClick={() => {
@@ -252,11 +258,7 @@ export default function RoomTypesFormCreate({
         >
           Cancel
         </button>
-        <button
-          className={stylesForm.submitBtn}
-          type="submit"
-          disabled={loading}
-        >
+        <button className={styles.submitBtn} type="submit" disabled={loading}>
           {loading ? "Creating..." : "Create"}
         </button>
       </menu>
