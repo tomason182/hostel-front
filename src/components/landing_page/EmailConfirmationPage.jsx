eb37f9b23b0b1e0cc3fa082f983ae61f12944611
tomason182 <elotro182@hotmail.com>
2024-10-29 09:55:30 -0300

    add: style email confirm page

--- src/components/landing_page/EmailConfirmationPage.jsx
+++ src/components/landing_page/EmailConfirmationPage.jsx
@@ -1,12 +1,26 @@
+import styles from "../../styles/EmailConfirmationPage.module.css";
+import { Link } from "react-router-dom";
+
 export default function EmailConfirmationPage() {
   return (
-    <div>
+    <div className={styles.mainContent}>
       <h1>Confirm Your Email Address</h1>
       <p>
         We&apos;ve sent an email to your inbox with a confirmation link. Please
         check your email and click the link to complete your account setup. If
         you don&apos;t see it within a few minutes, check your spam or junk
         folder.
+      </p>
+
+      <p>
+        Still having trouble?&nbsp;
+        <Link to="#" className={styles.footerLink}>
+          Resend confirmation email
+        </Link>
+        &nbsp;or&nbsp;
+        <Link to="#" className={styles.footerLink}>
+          Contact support
+        </Link>
       </p>
     </div>
   );
