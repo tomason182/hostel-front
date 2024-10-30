28477c10960ff1a22e923c201d869ba053540d45
tomason182 <elotro182@hotmail.com>
2024-10-30 18:30:35 -0300

    add: style error message

--- src/components/error_page/ErrorComponent.jsx
+++ src/components/error_page/ErrorComponent.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
-
+import styles from "../../styles/ErrorComponent.module.css";
 export default function ErrorComponent({ errors }) {
   const listErrors = errors.map((error, index) => (
     <li key={index}>{error.msg}</li>
   ));
-  return <ul className="errorMsg">{listErrors}</ul>;
+  return <ul className={styles.errorMessage}>{listErrors}</ul>;
 }
 
 ErrorComponent.propTypes = {
