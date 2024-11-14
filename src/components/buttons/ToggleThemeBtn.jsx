b8c70e0c6837c8fd21b7481eb6dda6d66290e488
tomason182 <elotro182@hotmail.com>
2024-11-14 15:42:23 -0300

    fix: sun svg image on firefox

--- src/components/buttons/ToggleThemeBtn.jsx
+++ src/components/buttons/ToggleThemeBtn.jsx
@@ -9,22 +9,38 @@ function ToggleThemeBtn() {
   }
 
   return (
-    <>
-      <button className={styles.toggleBtn} onClick={handleDarkMode}>
+    <button className={styles.toggleBtn} onClick={handleDarkMode}>
+      {isDarkMode ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width={24}
-          height={24}
+          width="24"
+          height="24"
           viewBox="0 0 24 24"
-          className={`${styles.themeSwitch} ${
-            isDarkMode ? styles.darkTheme : styles.lightTheme
-          }`}
+          fill="none"
+          stroke="#000000"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
-          <path></path>
-          {!isDarkMode && <circle></circle>}
+          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
         </svg>
-      </button>
-    </>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000000"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="12" cy="12" r="5" />
+          <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+        </svg>
+      )}
+    </button>
   );
 }
 
