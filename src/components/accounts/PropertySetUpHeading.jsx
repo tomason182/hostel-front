75a9a0a3aeff7fc37bdde6fd8cc76ae7aacf2922
tomason182 <elotro182@hotmail.com>
2024-11-26 20:21:04 -0300

    add: include notice in google sign up

--- src/components/accounts/PropertySetUpHeading.jsx
+++ src/components/accounts/PropertySetUpHeading.jsx
@@ -3,8 +3,12 @@ import styles from "../../styles/RegistrationHeading.module.css";
 export default function PropertySetUpHeading() {
   return (
     <div className={styles.mainContainer}>
-      <h2>Your Account is Almost Set Up</h2>
-      <p>Please, enter your property name</p>
+      <h2>Complete Your Registration</h2>
+      <p>
+        To create your account, please provide your property name and agree to
+        our terms and conditions.
+        <strong> Your data will not be saved if you leave this page.</strong>
+      </p>
     </div>
   );
 }
